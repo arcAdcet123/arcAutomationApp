1e11261a1ebf62bb236fd2cebcd866f19636ec95
Mark-1707 <shiroteomkar6@gmail.com>
2020-09-23 02:54:23 +0530

    Google Sign-in

--- app/src/main/java/com/example/automation_app/MainActivity.java
+++ app/src/main/java/com/example/automation_app/MainActivity.java
@@ -3,19 +3,37 @@ package com.example.automation_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button on,off;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        on=findViewById(R.id.BUTTON_ON);
+        off=findViewById(R.id.BUTTON_OFF);
 
-        myRef.setValue("Hall");
+        on.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("STATUS_ON");
+                myRef.setValue(1);
+            }
+        });
+
+        off.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("STATUS_OFF");
+                myRef.setValue(0);
+            }
+        });
     }
 }
